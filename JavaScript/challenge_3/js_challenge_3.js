157b96fadc2e9e5f0f895549066342da1f27809b
Anish <anishsingh1452002@gmail.com>
2021-11-02 19:11:39 +0530

    Modified challenge 3

---
+++ JavaScript/challenge_3/js_challenge_3.js
@@ -0,0 +1,6 @@
+let image = document.createElement('img');
+image.src = "/challenge_3/img/rock1.PNG";
+image.width = 100;
+image.height = 100;
+let div_r = document.getElementById('rock');
+div_r.appendChild(image);
