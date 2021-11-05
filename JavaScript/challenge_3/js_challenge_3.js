4bd9a65b836d5be7c70384cdfc1a1cef5316a711
Anish <anishsingh1452002@gmail.com>
2021-11-05 22:39:22 +0530

    JS-CH-3

--- JavaScript/challenge_3/js_challenge_3.js
+++ JavaScript/challenge_3/js_challenge_3.js
@@ -21,3 +21,30 @@ image_scissors.height = 150;
 image_scissors.width = 150;
 let div_s = document.getElementById('scissors');
 div_s.appendChild(image_scissors);
+
+//return the object that is usersChoice in this case ... observe we have written rpsGame(this) in html onclick attribute 
+function rpsGame(userChoice)
+{
+    console.log("user",userChoice.id)
+    var humanChoice, botChoice;
+    //humanChoice = userChoice.id;
+    // random computer input between 0-2
+    botChoice = randomComputerInput(); 
+    console.log("Bot",botChoice);
+    //get to know the results ---> results should be in a form of array === [1,0] or [0.5,0.5] or [0,1] humanScore and botScore respectively
+    var results = decideWinner(humanChoice,botChoice);
+    //once you have the results you need to print a a message
+    //message = outputMessage(results) //it will return in a form of JS object === {message: "Won/Lost/Tie" ; color: "Green/Red/Grey"}
+    //we also have to manipulate the frontend 
+    // rpsfrontend(humanChoice,botChoice,message)
+}
+
+function randomComputerInput()
+{
+    return ["rock","paper","scissors"][Math.floor(Math.random()*3)]
+}
+
+function decideWinner(yourChoice,computerChoice)
+{
+
+}
