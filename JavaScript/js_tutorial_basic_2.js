132a6fcabd05d758bb0d18b30ff73976f021a16c
Anish <anishsingh1452002@gmail.com>
2021-11-18 08:54:16 +0530

    Updates

---
+++ JavaScript/js_tutorial_basic_2.js
@@ -0,0 +1,15 @@
+//switch statements --- same as cpp
+//when a lot of cases needs to be checked ... better to use switch case
+//switch case follows fall pattern thats why add break when required 
+let _age_ = 20;
+switch(_age_)
+{
+    case 20:
+        console.log("You are 20");
+        break;
+    case 21:
+        console.log("You are 21.");
+        break;
+    default:
+        console.log("Neither 20 nor 21");
+}
