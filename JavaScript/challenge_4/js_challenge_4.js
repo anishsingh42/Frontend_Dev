88ed7bb9124e6d4793d3212149e4af9942c1f39a
Anish <anishsingh1452002@gmail.com>
2022-01-01 19:14:40 +0530

    challenge 4 modified

--- JavaScript/challenge_4/js_challenge_4.js
+++ JavaScript/challenge_4/js_challenge_4.js
@@ -1,1 +1,15 @@
- 
+let numberButtons = document.getElementsByClassName('JSnumber');
+console.log(numberButtons);
+
+let operatorButtons = document.getElementsByClassName('JSoperator');
+console.log(operatorButtons);
+
+let ACButton = document.getElementsByClassName('JSac');
+console.log(ACButton);
+
+let delelteButton = document.getElementsByClassName('JSdelete');
+console.log(delelteButton);
+
+let equalButton = document.getElementsByClassName('JSequals');
+console.log(equalButton);
+
