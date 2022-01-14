4f1bf0ad8957698c1203b1b9d70912a4d8d1f90b
Anish <anishsingh1452002@gmail.com>
2022-01-14 23:07:13 +0530

    To-do list 70% done

--- JavaScript/challenge_6/domPractice.js
+++ JavaScript/challenge_6/domPractice.js
@@ -4,6 +4,10 @@ let itemList = document.getElementById("items-list")
 //listen for the click event for the button
 addBtn.addEventListener("click", addItems);
 
+//delete the task
+itemList.addEventListener('click', deleteTask);
+
+
 function addItems(e) {
   //fetch the value at the inputText
   let newItem = inputText.value;
@@ -16,10 +20,37 @@ function addItems(e) {
     //styling to the task-list
     taskList.style.borderBottom = '0.25px solid rgba(51, 93, 45, 1)'
     taskList.style.borderRadius = '4px';
+
+    //create a deletebtn 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = "delete-btn";
+    deleteBtn.appendChild(document.createTextNode('x'));
+    console.log(deleteBtn);
+
+    //styling the deleteBtn
+    deleteBtn.style.float = 'right';
+    // deleteBtn.style.position = 'fixed'
     
     console.log(taskList);
     //inside this newly created list add newItem
     taskList.appendChild(document.createTextNode(newItem));
+
+    //append it into the taskList
+    taskList.appendChild(deleteBtn);
     itemList.appendChild(taskList);
+  }
+}
+
+
+function deleteTask(e)
+{
+  //we are listening for a event in the itemList therefore anywhere you click it might delete the task 
+  if(e.target.classList.contains('delete-btn'))
+  {
+    // console.log(1);
+    //the li is the childNode to the div which has the id item-list ... that is div is the parent and li being add is the child
+    let liChild = e.target.parentElement;
+    // console.log(liChild);
+    itemList.remove(liChild);
   }
 }
